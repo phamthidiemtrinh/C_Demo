9f61f576c8780cf4cdd45aac83fe4708f93dabd6
phamthidiemtrinh <diemtrinh.rhymt@gmail.com>
2017-09-25 14:14:57 +0700

    new file:   level2/Level1_hello.c 	new file:   level2/README.md

---
+++ level2/Level1_hello.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+void main ()
+  {
+   int a;
+   float b;
+   char c;
+   printf("nhap vao so nguyen : \n ");
+   scanf("%d",&a);
+   printf("nhap so thuc : \n");
+   scanf("%f",&b);
+   //fflush(stdin);
+   printf("nhap vao ky tu : \n");
+   scanf("%c",&c);
+   printf("\n a=%d   , b=%f   , C=%c",a,b,c);
+   
+}
